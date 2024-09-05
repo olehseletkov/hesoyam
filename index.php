e27979308338375d003d90f7f335a94cfa26e1f2
olehseletkov <52956470+olehseletkov@users.noreply.github.com>
2024-09-05 12:45:54 +0200

    gg
    gg

---
+++ index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="css/style_index.css">
+	<title>HESOYAM</title>
+</head>
+<body>
+	<div id="header_container"> 
+		<div id="header">
+			<a href="index.html"><div id="logo"></div>
+			</a>
+		
+		</div>
+		
+	</div>
+</div>
+
+
+	<div id="container">
+		<div id="left_bar">	
+			<div id="nav_bar">
+			<a href="index.html">Home</a>
+
+			<a href="shop.html">Shop</a>
+
+			<a href="aboutUs.html">About us</a>
+
+			<a href="contact.html">Contact</a>
+			<a href="copyright.html">Copyright</a>
+
+		</div>
+		</div>
+		
+		<div id="content">
+		<div id="intro">
+				<a class="intro_1" href="orange.html"><div>			
+					<!-- <img src="./media/images/intro_1.png"> -->
+		
+					</div></a>
+
+				<a class="intro_2" href="apple.html"><div>
+						<!-- <img src="./media/images/intro_2.png"> -->
+				</div>
+			</a>				
+				
+		</div>
+		</div>
+		<div id="right_bar">
+				<a href="contact.html">Contact</a>
+				<a href="ad_1.html"><img src="media/images/AD/wxElectro.png"></a>
+		</div>
+		
+		</div>	
+		
+	
+	
+	<div id="footer">
+		By Oleh Seletkov | tel. +1234567890
+ 
+		
+<?php
+	
+	echo time('h:i:s');
+?>
+		
+	</div>
+</body>
+</html>
